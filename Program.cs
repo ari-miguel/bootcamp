28eb3d6f44d8b9cc966dbc63ce6482c490979010
ari-miguel <127797279+ari-miguel@users.noreply.github.com>
2023-03-13 22:51:21 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+String? nombre;
+
+Console.WriteLine("Escriba su nombre: ");
+nombre = Console.ReadLine();
+Console.WriteLine(nombre);
+
+DateTime fecha = DateTime.Now;
+
+Console.WriteLine(fecha);
+
+
+
+
